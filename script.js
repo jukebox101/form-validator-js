e091b59e966ca642b5ddb57ee858d37011fac571
jukebox101 <leonora.squires@gmail.com>
2021-01-23 23:01:46 -0500

    check input length function

--- script.js
+++ script.js
@@ -39,7 +39,23 @@ function checkRequired(inputArr) {
     });
   }
 
-  
+  // Check input length
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+      showError(
+        input,
+        `${getFieldName(input)} must be at least ${min} characters`
+      );
+    } else if (input.value.length > max) {
+      showError(
+        input,
+        `${getFieldName(input)} must be less than ${max} characters`
+      );
+    } else {
+      showSuccess(input);
+    }
+  }
+
 //Event listeners
 form.addEventListener('submit', function(e) {
     e.preventDefault();
